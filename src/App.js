629da8e5e1c97899fc3c146542d13405c5f91325
anjuinterwork <anju@interwork.biz>
2019-09-28 15:45:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React ,{Component}from 'react';
+import logo from './logo.svg';
+import  _   from 'lodash'
+import './App.css';
+import SearchBar from './component/search_bar'
+import YTSearch from 'youtube-api-search'
+import VideoList from './component/video_list'
+import VideoDetail from './component/video_detail'
+const API_KEY=process.env.REACT_APP_YOUTUBE_API_KEY;
+
+class App extends Component{
+  constructor(props){
+    super(props)
+
+  this.state={
+    videos:[],
+    selectedVideo:null
+  }
+  this.videoSearch('icecream')
+
+}
+
+videoSearch(term){ 
+  YTSearch({key:API_KEY,term :term},(videos)=>{
+    this.setState(
+      {videos:videos,
+     selectedVideo:videos[0]});
+    });
+}
+
+render() {
+ 
+  const videoSearch = _.debounce((term)=>{this.videoSearch(term)},400);
+  return (
+    <div className="container-fluid">
+      
+      <div>
+        <div className="heading">Video streaming</div>
+       <SearchBar onSearchTermChange={videoSearch}/>
+      </div>
+         <div className="row">
+              <div  className="col-md-8  coloumn1">
+              
+              <VideoDetail video={this.state.selectedVideo}/>  
+              </div>
+              <div className="col-md-4 coloumn2">
+              <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+              videos={this.state.videos}/>
+             </div>
+         </div>
+     </div>
+    );
+}
+
+
+}
+
+
+export default App;
